2f1141713a5fe30d4764d2486ca5be0f47bad2d4
samuelbambu <samuel@bambucode.com>
2016-06-20 16:43:18 -0600

    analytics and clicky

--- statusAplication/static/js/main.js
+++ statusAplication/static/js/main.js
@@ -1,7 +1,7 @@
 const numberOfDays = 7;
 var daysList = { 0:"Dom",1:"Lun",2:"Mar",3:"Mie",4:"Jue",5:"Vie",6:"Sab" }
 getLastNumberOfDays = function(){ //returns a list of string representations (ex: Lun 20  )
-                                  //of the last specified days by numberOfDays 
+                                  //of the last specified days by numberOfDays
   var currentDate =  new Date().getTime() ;
   var days = _.range(0, numberOfDays);
   var timeStamps = _.map(//timestaps of last 5 days
@@ -20,6 +20,19 @@ getLastNumberOfDays = function(){ //returns a list of string representations (ex
   return dates;
 }
 
+//Google Analytics
+var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-94028-8']);
+      _gaq.push(['_setDomainName', 'www.eleventa.com']);
+      _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+//
 const mainApp = new appController();
 mainApp.attachRenderingElement( document.getElementById("app"));
 mainApp.fetchAllHosts();
