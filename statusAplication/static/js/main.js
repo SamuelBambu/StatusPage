0883f4ba65f69eafddcf94f924efcdd0fb953b7b
samuelbambu <samuel@bambucode.com>
2016-06-20 16:50:32 -0600

    try to fic GA y clicky

--- statusAplication/static/js/main.js
+++ statusAplication/static/js/main.js
@@ -19,20 +19,6 @@ getLastNumberOfDays = function(){ //returns a list of string representations (ex
   )
   return dates;
 }
-
-//Google Analytics
-var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-94028-8']);
-      _gaq.push(['_setDomainName', 'www.eleventa.com']);
-      _gaq.push(['_setAllowLinker', true]);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-//
 const mainApp = new appController();
 mainApp.attachRenderingElement( document.getElementById("app"));
 mainApp.fetchAllHosts();
